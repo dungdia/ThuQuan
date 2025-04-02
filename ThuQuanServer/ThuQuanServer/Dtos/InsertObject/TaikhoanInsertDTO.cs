8407bfc0310164c118702eedfbaff4e326663e0f
Windows 10 <hoangdungnhocom@gmail.com>
2025-04-02 15:16:59 +0700

    login & ingnore null props in add method of dbcontext & change verify passsword param localtion

---
+++ ThuQuanServer/ThuQuanServer/Dtos/InsertObject/TaikhoanInsertDTO.cs
@@ -0,0 +1,8 @@
+﻿using ThuQuanServer.Dtos.Request;
+
+namespace ThuQuanServer.Dtos.InsertObject;
+
+public class TaikhoanInsertDTO : TaiKhoanRequestDto
+{
+    public DateTime? NgayThamGia { get; set; } = DateTime.Now;
+}
