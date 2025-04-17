4a621c3414eb04f60e3f6d3ddcc3887ccec9843d
Windows 10 <hoangdungnhocom@gmail.com>
2025-04-17 16:25:22 +0700

    phieu dat, chiet tiet phieu dat dto to response api with list

---
+++ ThuQuanServer/ThuQuanServer/Dtos/Response/ChiTietPhieuDatResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ThuQuanServer.Dtos.Response;
+
+public class ChiTietPhieuDatResponseDto
+{
+    public int Id_PhieuDat { get; set; }
+    public int Id_VatDung { get; set; }
+    public VatDung VatDung { get; set; }
+}
