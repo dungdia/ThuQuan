7fd8bb36f0f85625df1c9620db606dad10d858b8
phusomnia <tuanphu2004thcs@gmail.com>
2025-04-29 21:34:33 +0700

    Chức năng check lịch sử vào, bổ sung xử lý vi phạm sau

---
+++ ThuQuanServer/ThuQuanServer/Endpoints/LichSuEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using MySqlX.XDevAPI.Common;
+using ThuQuanServer.ApplicationContext;
+using ThuQuanServer.Dtos;
+using ThuQuanServer.Interfaces;
+
+namespace ThuQuanServer.Endpoints;
+
+public static class  LichSuEndpoint
+{
+    public static void MapLichSuEndpoints(this IEndpointRouteBuilder app)
+    {
+        var lichSuRepository = app.ServiceProvider.GetRequiredService<ILichSuRepository>();
+        var groupName = "Lich Su";
+        
+        app.MapGet("/GetAllLichSu", () =>
+        {
+            var result = lichSuRepository.GetLichSu();
+            
+            if (!result.Any())
+            {
+                return Results.BadRequest("Không thể lấy danh sách lịch sử");
+            }
+
+            return Results.Ok(result);
+        }).WithTags(groupName);
+        
+        app.MapPost("/CheckLichSuVao", (int? IdThanhVien) =>
+        {
+            if(!lichSuRepository.CheckLichSuVao(IdThanhVien)){
+                return Results.BadRequest("Lỗi xử lý lịch sử vào");
+            }
+            
+            return Results.Ok(new 
+            {
+                Success = true,
+                Message = "Mời vào!",
+            });
+        }).WithTags(groupName);
+    }
+}
--- ThuQuanServer/ThuQuanServer/Models/LichSu.cs
+++ ThuQuanServer/ThuQuanServer/Models/LichSu.cs
@@ -2,5 +2,7 @@
 
 public class LichSu
 {
-    
+    public int Id { get; set; }
+    public DateTime ThoiGianVao { get; set; }
+    public int Id_ThanhVien { get; set; }
 }
