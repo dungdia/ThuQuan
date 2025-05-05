f389a40448709d948c8410e733ef2821ddfdfb83
phusomnia <tuanphu2004thcs@gmail.com>
2025-05-05 17:08:49 +0700

    chỉnh cho panel nhìn cân đối tí

--- DesktopClient/DesktopClient/UI/Dialog/RegisterMemberDialog.Designer.cs
+++ DesktopClient/DesktopClient/UI/Dialog/RegisterMemberDialog.Designer.cs
@@ -41,8 +41,9 @@
             // 
             // confirmBtn
             // 
+            confirmBtn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             confirmBtn.BackColor = SystemColors.MenuHighlight;
-            confirmBtn.Location = new Point(471, 389);
+            confirmBtn.Location = new Point(226, 253);
             confirmBtn.Name = "confirmBtn";
             confirmBtn.Size = new Size(141, 40);
             confirmBtn.TabIndex = 0;
@@ -52,21 +53,24 @@
             // 
             // username_Input
             // 
-            username_Input.Location = new Point(227, 133);
+            username_Input.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            username_Input.Location = new Point(101, 65);
             username_Input.Name = "username_Input";
             username_Input.Size = new Size(200, 23);
             username_Input.TabIndex = 1;
             // 
             // email_Input
             // 
-            email_Input.Location = new Point(227, 182);
+            email_Input.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            email_Input.Location = new Point(101, 114);
             email_Input.Name = "email_Input";
             email_Input.Size = new Size(200, 23);
             email_Input.TabIndex = 2;
             // 
             // password_Input
             // 
-            password_Input.Location = new Point(227, 231);
+            password_Input.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            password_Input.Location = new Point(101, 163);
             password_Input.Name = "password_Input";
             password_Input.PasswordChar = '*';
             password_Input.Size = new Size(200, 23);
@@ -74,7 +78,8 @@
             // 
             // confirmPassword_Input
             // 
-            confirmPassword_Input.Location = new Point(227, 280);
+            confirmPassword_Input.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            confirmPassword_Input.Location = new Point(101, 212);
             confirmPassword_Input.Name = "confirmPassword_Input";
             confirmPassword_Input.PasswordChar = '*';
             confirmPassword_Input.Size = new Size(200, 23);
@@ -82,9 +87,10 @@
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label1.Location = new Point(227, 110);
+            label1.Location = new Point(101, 42);
             label1.Name = "label1";
             label1.Size = new Size(101, 20);
             label1.TabIndex = 5;
@@ -92,9 +98,10 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label2.Location = new Point(227, 159);
+            label2.Location = new Point(101, 91);
             label2.Name = "label2";
             label2.Size = new Size(47, 20);
             label2.TabIndex = 6;
@@ -102,9 +109,10 @@
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label3.Location = new Point(227, 208);
+            label3.Location = new Point(101, 140);
             label3.Name = "label3";
             label3.Size = new Size(75, 20);
             label3.TabIndex = 7;
@@ -112,9 +120,10 @@
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label4.Location = new Point(227, 257);
+            label4.Location = new Point(101, 189);
             label4.Name = "label4";
             label4.Size = new Size(141, 20);
             label4.TabIndex = 8;
@@ -124,7 +133,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(624, 441);
+            ClientSize = new Size(402, 325);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
