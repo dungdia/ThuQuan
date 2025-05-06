73ebf4ad4708d5373b4b413633268ec027d14ae7
Quangdung090 <ttramquangdung@gmail.com>
2025-05-06 17:49:21 +0700

    Member UI minor fixed

--- DesktopClient/DesktopClient/UI/Dialog/HistoryDialog.Designer.cs
+++ DesktopClient/DesktopClient/UI/Dialog/HistoryDialog.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             XemLichSu_Table = new DataGridView();
             search_Btn = new Button();
             month_cBox = new ComboBox();
@@ -47,25 +47,25 @@
             XemLichSu_Table.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             XemLichSu_Table.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             XemLichSu_Table.BackgroundColor = SystemColors.ButtonFace;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            XemLichSu_Table.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            XemLichSu_Table.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             XemLichSu_Table.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = SystemColors.Window;
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
-            XemLichSu_Table.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = SystemColors.Window;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.False;
+            XemLichSu_Table.DefaultCellStyle = dataGridViewCellStyle4;
             XemLichSu_Table.Dock = DockStyle.Bottom;
-            XemLichSu_Table.Location = new Point(0, 144);
+            XemLichSu_Table.Location = new Point(0, 192);
             XemLichSu_Table.Margin = new Padding(0);
             XemLichSu_Table.MultiSelect = false;
             XemLichSu_Table.Name = "XemLichSu_Table";
@@ -73,16 +73,17 @@
             XemLichSu_Table.RowHeadersVisible = false;
             XemLichSu_Table.RowHeadersWidth = 51;
             XemLichSu_Table.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            XemLichSu_Table.Size = new Size(800, 306);
+            XemLichSu_Table.Size = new Size(914, 408);
             XemLichSu_Table.TabIndex = 3;
             // 
             // search_Btn
             // 
-            search_Btn.Location = new Point(291, 71);
+            search_Btn.Location = new Point(333, 95);
+            search_Btn.Margin = new Padding(3, 4, 3, 4);
             search_Btn.Name = "search_Btn";
-            search_Btn.Size = new Size(75, 23);
+            search_Btn.Size = new Size(86, 31);
             search_Btn.TabIndex = 6;
-            search_Btn.Text = "tìm kiếm";
+            search_Btn.Text = "Tìm kiếm";
             search_Btn.UseVisualStyleBackColor = true;
             search_Btn.Click += SearchEvent;
             // 
@@ -91,9 +92,10 @@
             month_cBox.DropDownStyle = ComboBoxStyle.DropDownList;
             month_cBox.FormattingEnabled = true;
             month_cBox.Items.AddRange(new object[] { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" });
-            month_cBox.Location = new Point(12, 72);
+            month_cBox.Location = new Point(14, 96);
+            month_cBox.Margin = new Padding(3, 4, 3, 4);
             month_cBox.Name = "month_cBox";
-            month_cBox.Size = new Size(121, 23);
+            month_cBox.Size = new Size(138, 28);
             month_cBox.TabIndex = 7;
             // 
             // year_cBox
@@ -101,44 +103,46 @@
             year_cBox.DropDownStyle = ComboBoxStyle.DropDownList;
             year_cBox.FormattingEnabled = true;
             year_cBox.Items.AddRange(new object[] { "2024", "2025" });
-            year_cBox.Location = new Point(155, 72);
+            year_cBox.Location = new Point(177, 96);
+            year_cBox.Margin = new Padding(3, 4, 3, 4);
             year_cBox.Name = "year_cBox";
-            year_cBox.Size = new Size(121, 23);
+            year_cBox.Size = new Size(138, 28);
             year_cBox.TabIndex = 8;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 54);
+            label1.Location = new Point(14, 72);
             label1.Name = "label1";
-            label1.Size = new Size(40, 15);
+            label1.Size = new Size(50, 20);
             label1.TabIndex = 9;
             label1.Text = "Tháng";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(155, 54);
+            label2.Location = new Point(177, 72);
             label2.Name = "label2";
-            label2.Size = new Size(31, 15);
+            label2.Size = new Size(41, 20);
             label2.TabIndex = 10;
-            label2.Text = "năm";
+            label2.Text = "Năm";
             // 
             // reset_Btn
             // 
-            reset_Btn.Location = new Point(381, 71);
+            reset_Btn.Location = new Point(435, 95);
+            reset_Btn.Margin = new Padding(3, 4, 3, 4);
             reset_Btn.Name = "reset_Btn";
-            reset_Btn.Size = new Size(75, 23);
+            reset_Btn.Size = new Size(86, 31);
             reset_Btn.TabIndex = 11;
-            reset_Btn.Text = "reset";
+            reset_Btn.Text = "Làm mới";
             reset_Btn.UseVisualStyleBackColor = true;
             reset_Btn.Click += ResetEvent;
             // 
             // HistoryDialog
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(reset_Btn);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -146,6 +150,7 @@
             Controls.Add(month_cBox);
             Controls.Add(search_Btn);
             Controls.Add(XemLichSu_Table);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "HistoryDialog";
             Text = "HistoryDialog";
             ((System.ComponentModel.ISupportInitialize)XemLichSu_Table).EndInit();
